d0ccbd5f717f30e4e31ea76586f7a25c3f335762
StegosaurusLlama <95941355+StegosaurusLlama@users.noreply.github.com>
2026-04-07 20:46:29 -0500

    Initial test push

---
+++ flaskTest.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+	return "<a href='/test'><p>Hello, World!</p></a>"
+	
+@app.route("/test")
+def test():
+	return "<h1>TEST</h1>"
+	
+def getDatabase():
+	
