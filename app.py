54a3711e9339a651a108d1a22e7ec5a56fe1e2a8
StegosaurusLlama <95941355+StegosaurusLlama@users.noreply.github.com>
2026-04-08 21:29:34 -0500

    small testing changes

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "<a href='/test'><p>Hello, World!</p></a>"
+	return "<a href='/login'><p>Hello, World!</p></a>"
 	
 @app.route("/test")
 def test():
 	return "<h1>TEST</h1>"
-	
+
+@app.route("/login")
+def login():
+	username = "testing"
+	return render_template("log_in.html", name=username)
+
+@app.route("/login-submit", methods=["POST"])
+def login_attempt():
+	name = request.form.get("username")
+	print(name)
+	if name == "admin":
+		return "<h1>Hello Admin</h1>"
+	else:
+		return redirect(url_for("login"))
+
 def getDatabase():
 	pass
